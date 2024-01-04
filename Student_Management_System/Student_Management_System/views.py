e62f1a56f69929b4d0495e099e91d18d6a9280c0
kskhan1911 <68466116+kskhan1911@users.noreply.github.com>
2024-01-04 11:45:11 +0600

    Student-Management-System - Step-71

--- Student_Management_System/Student_Management_System/views.py
+++ Student_Management_System/Student_Management_System/views.py
@@ -5,6 +5,9 @@ from app.EmailBackEnd import EmailBackEnd
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from app.models import CustomUser
+
+
 def BASE(request):
     return render(request,'base.html')
 
@@ -24,7 +27,7 @@ def doLogin(request):
             if user_type == '1':
                 return redirect('hod_home')
             elif user_type == '2':
-                return HttpResponse('This is Staff Panel')
+                return redirect('staff_home')
             elif user_type == '3':
                 return HttpResponse('This is Student Panel')
             else:
@@ -38,3 +41,42 @@ def doLogin(request):
 def doLogout(request):
     logout(request)
     return redirect('login')
+
+
+@login_required(login_url='/')
+def PROFILE(request):
+    user = CustomUser.objects.get(id=request.user.id)
+
+    context = {
+        "user": user,
+    }
+    return render(request, 'profile.html', context)
+
+
+@login_required(login_url='/')
+def PROFILE_UPDATE(request):
+    if request.method == "POST":
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        # email = request.POST.get('email')
+        # username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(profile_pic)
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+
+            customuser.first_name = first_name
+            customuser.last_name = last_name
+
+            if password is not None and password != "":
+                customuser.set_password(password)
+            if profile_pic is not None and profile_pic != "":
+                customuser.profile_pic = profile_pic
+            customuser.save()
+            messages.success(request, 'Your Profile Updated Successfully !')
+            return redirect('profile')
+        except:
+            messages.error(request, 'Failed To Update Your Profile')
+
+    return render(request, 'profile.html')
--- Student_Management_System/app/admin.py
+++ Student_Management_System/app/admin.py
@@ -8,3 +8,33 @@ class UserModel(UserAdmin):
 
 
 admin.site.register(CustomUser, UserModel)
+
+# Course model Register
+admin.site.register(Course)
+
+# Session_Year model Register
+admin.site.register(Session_Year)
+
+# Student Model Register
+admin.site.register(Student)
+
+# Staff Model Register
+admin.site.register(Staff)
+
+# Subject Model Register
+admin.site.register(Subject)
+
+# Staff_Notification Model Register
+admin.site.register(Staff_Notification)
+
+# Staff_Leave Model Register
+admin.site.register(Staff_Leave)
+
+# Staff_Feedback Model Register
+admin.site.register(Staff_Feedback)
+
+# Attendance Model Register
+admin.site.register(Attendance)
+
+# Attendance_Report Model Register
+admin.site.register(Attendance_Report)
